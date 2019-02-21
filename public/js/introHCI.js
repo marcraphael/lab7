31c16af6dd562386cb8bd69dbba300c0563cba83
Marc Raphael <marc.raphael12@gmail.com>
2019-02-21 11:46:51 -0800

    final

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,14 @@ $(document).ready(function() {
 	initializePage();
 });
 
+function clickLike() {
+	ga("send", "event", "like", "click");
+};
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(clickLike);
 }
